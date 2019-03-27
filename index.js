b1d8363a2e43456ed06595bc0cb4a0fbb9d24f9e
William <willy91330@gmail.com>
2019-03-27 15:23:48 +0100

    Responsive menu + navbar

---
+++ index.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+    var state = false,
+        links = $(".navbar-responsive__link");
+    $("#nav-icon3").click(function () {
+        $(this).toggleClass("open");
+        if (!state) {
+            $(".navbar-responsive").css("transform", "translate3d(0,0,0)");
+            state = true;
+            $(".body-class").addClass("block-scroll");
+        } else {
+            $(".navbar-responsive").css(
+                "transform",
+                "translate3d(-100%,0,0)"
+            );
+            state = false;
+            $(".body-class").removeClass("block-scroll");
+        }
+    });
+    $.each(links, function (index, value) {
+        value.addEventListener("click", function () {
+            if (!state) {
+                $(".navbar-responsive").css("transform", "translate3d(0,0,0)");
+                state = true;
+            } else {
+                $(".navbar-responsive").css(
+                    "transform",
+                    "translate3d(-100%,0,0)"
+                );
+                state = false;
+            }
+            $("#nav-icon3").removeClass("open");
+        });
+    });
+
+    $(window).scroll(function () {
+        var sc = $(window).scrollTop();
+        if (sc > 100) {
+            $(".logo").addClass("logo-scroll");
+            $("#main-navbar").addClass("navbar-scroll");
+        } else {
+            $(".logo").removeClass("logo-scroll");
+            $("#main-navbar").removeClass("navbar-scroll");
+        }
+    });
+});
