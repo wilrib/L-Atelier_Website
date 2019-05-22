0738fadb44b0b381691900fc9323a6316455c2af
William <willy91330@gmail.com>
2019-05-22 11:54:59 +0200

    modif responsive banquet

--- js/index.js
+++
@@ -1,8 +0,0 @@
-// $(window).scroll(function () {
-//     var sc = $(window).scrollTop()
-//     if (sc > 300) {
-//         $("#main-navbar").addClass("navbar-scroll")
-//     } else {
-//         $("#main-navbar").removeClass("navbar-scroll")
-//     }
-// });
